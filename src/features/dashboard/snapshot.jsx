c4d8643a11fda3e7bb74941223a5bfc3d0dcb84b
kalyani-wal <kalyani.s@westagilelabs.com>
2022-05-10 15:29:58 +0530

    minor change

--- src/features/dashboard/snapshot.jsx
+++ src/features/dashboard/snapshot.jsx
@@ -5,8 +5,9 @@ const Snapshot = () => {
     <div className="snapshot-section">
       <h3>Here's today in a snapshot.</h3>
       <div className="duration">
+        {/* add class "active" on click of span  */}
         <span>12m</span>
-        <span>30d</span>
+        <span className="active">30d</span>
         <span>7d</span>
         <span>24h</span>
       </div>
